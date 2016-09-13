9d090f0b07fce25e031b4f32273e7360c6baf356
Rahul Bhola <me@rahulbhola.in>
2016-09-14 01:04:19 +0530

    Common http extentions added

--- api.js
+++ api.js
@@ -1,11 +1,27 @@
 function generateRoute(app,context,models) {
   for(var endpoints in context.rules){
     var routeConfig = context.rules[endpoints];
-    app.get(endpoints,function(req,res,next) {
-      var cfg = context.rules[req.url];
-      var model = models[cfg.db];
-      require(cfg.mixin).get(req,res);
-    });
+    app
+      .get(endpoints,function(req,res,next) {
+        var cfg = context.rules[req.url];
+        var model = models[cfg.db];
+        require(cfg.mixin).get(req,res);
+      })
+      .put(endpoints,function(req,res,next) {
+        var cfg = context.rules[req.url];
+        var model = models[cfg.db];
+        require(cfg.mixin).put(req,res);
+      })
+      .post(endpoints,function(req,res,next) {
+        var cfg = context.rules[req.url];
+        var model = models[cfg.db];
+        require(cfg.mixin).post(req,res);
+      })
+      .delete(endpoints,function(req,res,next) {
+        var cfg = context.rules[req.url];
+        var model = models[cfg.db];
+        require(cfg.mixin).delete(req,res);
+      });
   }
 }
 
--- mixin/listViewMixin.js
+++ mixin/listViewMixin.js
@@ -4,4 +4,16 @@ router.get = function(req,res) {
   res.send("Testing");
 }
 
+router.put = function(req,res) {
+  res.send("Testing");
+}
+
+router.post = function(req,res) {
+  res.send("Testing");
+}
+
+router.delete = function(req,res) {
+  res.send("Testing");
+}
+
 module.exports = router;
