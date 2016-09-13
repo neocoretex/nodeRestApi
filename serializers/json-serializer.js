28ced1581f6cc9d8ddd78ba93bb32444e150e030
Gurukiran097 <gurukiran.v97@gmail.com>
2016-09-14 03:06:23 +0530

    Update json-serializer.js

--- serializers/json-serializer.js
+++ serializers/json-serializer.js
@@ -1,18 +1,30 @@
+var fs = require('fs');
+
+function jsonObject(fileName) {
+  var content = fs.readFileSync(fileName,'utf8');
+  return JSON.parse(content);
+}
+
+var cfg = jsonObject('../api.json');
+
 function serialize(req,res) {
   if(req.err) {
-    res.status(422).json(res.err);
+    res.status(422).json({errors:{detail:res.err}});
   }else{
     req.data.forEach(function(data,i) {
       var temp = data[i];
       req.data[i] = {
         _id: temp.id,
-        type: 'data',
-        attributes:{
-        }
+        type: 'data'
       };
+      for(field in cfg.rules['/'+req.model].fields){
+        data[i].attributes[field] = temp.field;
+      }
+      
     });
-    res.status(200).json({data:req.data});
+    res.status(200).json({data:JSON.stringify(req.data)});
   }
 }
 
 module.exports = serialize;
+
