c8481c9b021fdab7844dc3cc6c02c62f22ffd15a
Rahul Bhola <me@rahulbhola.in>
2016-09-14 03:16:13 +0530

    context passed in req.context

--- api.js
+++ api.js
@@ -6,23 +6,27 @@ function generateRoute(app,context,models) {
         var cfg = context.rules[req.url];
         req.model = models[cfg.db];
         req.modelName = cfg.modelName;
+        req.context = cfg;
         require(cfg.mixin).get(req,res,require(context.serializer));
       })
       .put(endpoints,function(req,res,next) {
         var cfg = context.rules[req.url];
         req.model = models[cfg.db];
+        req.context = cfg;
         require(cfg.mixin).put(req);
         require(context.context.serializer)(req,res);
       })
       .post(endpoints,function(req,res,next) {
         var cfg = context.rules[req.url];
         req.model = models[cfg.db];
+        req.context = cfg;
         require(cfg.mixin).post(req);
         require(context.context.serializer)(req,res);
       })
       .delete(endpoints,function(req,res,next) {
         var cfg = context.rules[req.url];
         req.model = models[cfg.db];
+        req.context = cfg;
         require(cfg.mixin).delete(req);
         require(context.context.serializer)(req,res);
       });
