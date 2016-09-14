c2ac9bd26c1dd4ad8119078d73cbe96efcfabf47
Rahul Bhola <me@rahulbhola.in>
2016-09-15 01:19:10 +0530

    put req array

--- mixin/listViewMixin.js
+++ mixin/listViewMixin.js
@@ -21,7 +21,7 @@ router.put = function(req,res,next) {
   var dataInstance = new req.model(req.body);
   dataInstance.save(function(err,data) {
     req.err = err;
-    req.data = data;
+    req.data = [data];
     next(req,res);
   });
 }
