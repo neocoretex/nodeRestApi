fc1b0756886a54431460b801515d433658041c84
Rahul Bhola <me@rahulbhola.in>
2016-09-15 00:42:32 +0530

    Proper body parsing in post

--- mixin/listViewMixin.js
+++ mixin/listViewMixin.js
@@ -9,7 +9,7 @@ router.get = function(req,res,next) {
 }
 
 router.post = function(req,res,next) {
-  var dataInstance = new req.model({email:"tek",content:"akjsdhf"});
+  var dataInstance = new req.model(req.body);
   dataInstance.save(function(err,data) {
     req.err = err;
     req.data = data;
